7a7bc9c3a02bbbb57f84f4a945b84764c2a07e69
Abdalmagid Elmaghbub <47678429+abalmagid@users.noreply.github.com>
2020-01-10 20:05:47 +0200

    First File is Done

---
+++ Eggs/FirstFile.swift
@@ -0,0 +1,15 @@
+//
+//  FirstFile.swift
+//  Eggs
+//
+//  Created by Abdalmagid elmaghbub on 1/10/20.
+//  Copyright © 2020 Abdalmagid elmaghbub. All rights reserved.
+//
+
+import Foundation
+
+class FirstFile{
+    var x = 0
+    var y = 2
+    
+}
